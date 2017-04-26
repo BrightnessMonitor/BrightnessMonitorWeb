6213bb1a5329d51608ba799d22fa3c5076c78752
Steffen Exler <Steffen.Exler@gmail.com>
2017-04-26 21:05:10 +0200

    update heroku deployment button

--- heroku-env-default.sh
+++ heroku-env-default.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# log into python env
-source env/bin/activate
-
 # set SECRET_KEY
 heroku config:set SECRET_KEY=22222222222
 
 # set DJANGO_SETTINGS_MODULE
 heroku config:set DJANGO_SETTINGS_MODULE=BrightnessMonitorWeb.settings.production
+
+# set WEB_CONCURRENCY
+heroku config:set WEB_CONCURRENCY=2
+
+# disable collectstatic
+heroku config:set DISABLE_COLLECTSTATIC=1
